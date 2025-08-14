86c0523561ecc8397cb2f231daec8a9c07ab0ad4
kage-96 <61382022+kage-96@users.noreply.github.com>
2025-08-14 20:44:55 +0900

    課題4の実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { BlogDetail } from './Pages/BlogDetail'
 import { Layout } from './Components/Layout'
+import { Contact } from './Pages/Contact'
 function App() {
 
   return (
@@ -11,7 +12,8 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path={`/posts/:id`} element={<BlogDetail />} />
-            </Route>
+            <Route path='/contact/' element={<Contact />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
